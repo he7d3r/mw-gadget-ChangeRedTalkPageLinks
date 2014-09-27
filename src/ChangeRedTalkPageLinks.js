ca4a8473f03400657e8ea641cd0c7f6fc8a5b359
Helder <he7d3r@gmail.com>
2014-09-27 20:48:56 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/ChangeRedTalkPageLinks.js
+++ src/ChangeRedTalkPageLinks.js
@@ -2,16 +2,16 @@
  * Make red links to talk pages behave like action=edit&section=new ([[bugzilla:22416]])
  */
 ( function ( $ ) {
-'use strict';
+	'use strict';
 
-var changeRedTalkPageLinks = function(){
-	$('#ca-talk.new')
-		// .removeClass('new')
-		.find('a')
-			.attr('href', function(i,href){
-				return href.replace('redlink=1', 'section=new');
-			});
-};
-$(changeRedTalkPageLinks);
+	var changeRedTalkPageLinks = function () {
+		$('#ca-talk.new')
+			// .removeClass('new')
+			.find('a')
+				.attr('href', function (i, href) {
+					return href.replace('redlink=1', 'section=new');
+				});
+	};
+	$( changeRedTalkPageLinks );
 
 }( jQuery ) );
